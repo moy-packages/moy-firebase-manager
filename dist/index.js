84e1bec4f50aeb680e07250884ac342a9451d5c9
Gavaar <oxspit@gmail.com>
2022-08-10 18:42:24 +0200

    1.1.12 better published files

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MoyFirestoreMock = exports.MoyFirestoreManager = void 0;
-var firestore_manager_1 = require("./firestore-manager");
-Object.defineProperty(exports, "MoyFirestoreManager", { enumerable: true, get: function () { return firestore_manager_1.MoyFirestoreManager; } });
-var testing_1 = require("./testing");
-Object.defineProperty(exports, "MoyFirestoreMock", { enumerable: true, get: function () { return testing_1.MoyFirestoreMock; } });
